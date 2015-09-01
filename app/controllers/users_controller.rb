b15f50f4e79d6a89a34aa56d8ea548e992c94e49
Myk Klemme <mykklemme@gmail.com>
2015-09-01 15:22:17 -0700

    fixed logic in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,9 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     if user.blank?
-      render "templates/widgets/profile", locals: { user: user }, layout: "application"
-    else
       render "templates/widgets/profile-404", layout: "application"
+    else
+      render "templates/widgets/profile", locals: { user: user }, layout: "application"
     end
   end
 end
